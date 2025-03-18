3b34e287aa76810d80595525813c72cd693756d1
gonca <gonca@GONCAYEIL>
2025-03-19 01:23:26 +0300

    Proje dosyası ekle.

---
+++ NESNE.ödev--2/NESNE ÖDEV GONCA 2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NESNE.ödev__2
+{
+    public partial class çekiliş : Form
+    {
+        public çekiliş()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HashSet<int> kullaniciSayilari = new HashSet<int>
+                {
+                    int.Parse(txtSayi1.Text),
+                    int.Parse(txtSayi2.Text),
+                    int.Parse(txtSayi3.Text),
+
+                };
+                if (kullaniciSayilari.Any(s => s < 9 || s > 39))
+                {
+                    TextBox.Show("Lütfen 9 ile 39 arasında 3 farklı sayı girin");
+                    return;
+                }
+                int buyukOdulSayisi = 0;
+                Random rnd = new
+                    Random();
+                for (int i = 0; i < 1000; i++)
+                {
+                    HashSet<int>
+                        cekilisSayilari = new
+                        HashSet<int>();
+                    while (cekilisSayilari.Count < 3)
+                    {
+                        cekilisSayilari.Add(rnd.Next(9, 40));
+                    }
+                    if (cekilisSayilari.SetEquals(kullaniciSayilari))
+                    {
+                        buyukOdulSayisi++;
+                    }
+
+                }
+                lb1Sonuc.Text = $"Büyük Ödül{buyukOdulSayisi}kez kazanıldı!";
+                catch
+                {
+                TextBox.Show("Lütfen geçerli sayilar girin.");
+
+            }   }
+                    
+                
+        }
+    }
+}
---
+++ NESNE.ödev--2/çekiliş.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace NESNE.ödev__2
+{
+    partial class çekiliş
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(415, 235);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "tıkla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(415, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 20);
+            label1.TabIndex = 1;
+            label1.Text = "sonuç ekranı";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(109, 187);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(109, 288);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // çekiliş
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "çekiliş";
+            Text = "çekiliş";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
